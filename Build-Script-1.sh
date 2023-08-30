19894bf4ecb2aecfbe3132deca4719eee3721638
Kedmond24 <Kedmond24@live.com>
2023-08-30 14:32:40 -0400

    Initial Commit

---
+++ Build-Script-1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##############################################################################################################################
+# Created by Kevin Edmond.
+#
+# This script is used to run weekly system update checks and apply any outstanding updates. Once the updates are complete, the
+# number of updates applied will be count and recorded in a file named updates<current_date>.txt (ex. update04.20.23.txt).
+#
+# Please read README.md for full notes on script.
+#
+###############################################################################################################################
+
+
---
+++ README.md
@@ -0,0 +1,13 @@
+# Build Script 1
+
+---
+
+Created By Kevin Edmond
+
+Repository for Build Script 1 -
+
+### Instructions:
+
+### Script Logic:
+
+### Description:
